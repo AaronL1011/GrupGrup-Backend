3bce4caf54ee7f859c4f5de4a8e6dda73ce44934
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-07-23 21:47:36 +1000

    reverted last changes because they didnt fix anything

--- app.js
+++ app.js
@@ -16,12 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  next();
-});
-
 // Initial routes
 app.use('/', uploadRoute);
 app.use('/api/auth', authRoutes);
